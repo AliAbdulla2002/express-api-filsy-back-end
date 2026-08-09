5ec245459aff21e9d863066bc12f70e91f0cd994
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-09 14:16:48 +0300

    Add show funcion to the expense if the user clicked on the specific item will show

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -63,9 +63,30 @@ const deleteExpense = async function (req, res) {
   }
 }
 
+
+const show = async function (req, res) {
+  try {
+    const expense = await Expense.findById(req.params.expenseId).populate('owner')
+    
+    if (!expense) 
+      {
+      return res.status(404).json({ err: 'Expense not found' })
+    }
+    if (!expense.owner.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!")
+    }
+    
+    res.status(200).json(expense)
+  } catch (err) 
+  {
+    res.status(500).json({ err: err.message })
+  }
+}
+
 module.exports = {
   create,
   index,
   update,
   deleteExpense,
+  show,
 }
