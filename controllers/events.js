2df303e07815f30d5aeac4d3d70549f1ebad6a43
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-09 11:25:06 +0300

    Add the create function and exported

--- controllers/events.js
+++ controllers/events.js
@@ -1,3 +1,20 @@
 const express = require('express')
 const Event = require('../models/event.js')
 
+const create = async function (req, res) {
+  try {
+    req.body.owner = req.user._id
+    const event = await Event.create(req.body)
+    event._doc.owner = req.user
+    res.status(201).json(event)
+  } catch (err) 
+  {
+    res.status(500).json({ err: err.message })
+  }
+}
+
+
+module.exports = 
+{
+    create,
+}
